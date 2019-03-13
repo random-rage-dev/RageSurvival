72f4c93b57c56589be75e9ef57de14e233c0d23b
Marcel McJackson <the.second99@gmail.com>
2019-03-13 15:41:12 +0100

    inventory

--- client/cef/views/user_interface/script/script.js
+++ client/cef/views/user_interface/script/script.js
@@ -1,13 +1,26 @@
 "use strict";
-let test_item = {
-    name: "Pistol",
-    img: "../../../../source/img/weapon_combatpdw.png",
-    width: 3,
+let item_pdw = {
+    name: "combatpdw",
+    width: 4,
     height: 2
 };
+
+let item_hatchet = {
+    name: "hatchet",
+    width: 2,
+    height: 4
+};
+
+let item_parachute = {
+    name: "parachute",
+    width: 2,
+    height: 3
+};
+
 let Inventory = {
     selector: $("#inventory"),
     width: null,
+
     Generate: function(slots) {
         for (let i = 0; i < slots; i++) {
             console.log(i);
@@ -15,6 +28,7 @@ let Inventory = {
         }
         Inventory.width = $('#inventory').css('grid-template-columns').split(' ').length;
     },
+
     SetItem: function(item, x, y, flip = false) {
         let width = item.width;
         let height = item.height;
@@ -30,21 +44,26 @@ let Inventory = {
                     Inventory.selector.children().eq(Inventory.GetIndex(i, j)).addClass('used');
                 }
             }
+            Inventory.selector.children().eq(Inventory.GetIndex(x, y)).addClass("item item" + width + "x" + height + " " + item.name);
         } else {
             // hinweis
             return false;
         }
     },
+
     IsFree: function(x, y, width, height) {
         //Vertikale überprüfung hinzufügen
         for (let i = x; i < x + width; i++)
             for (let j = y; j < y + height; j++)
-                if (Inventory.selector.children().eq(Inventory.GetIndex(i, j)).hasClass('used')) return false;
+                if (Inventory.selector.children().eq(Inventory.GetIndex(i, j)).hasClass('used'))
+                    return false;
         return (x + width <= Inventory.width);
     },
+
     GetIndex: function(x, y) {
         return y * Inventory.width + x;
     },
+
     GetCoords: function(index) {
         let rowPos = Math.floor(index / Inventory.width);
         let colPos = index % Inventory.width;
@@ -54,10 +73,28 @@ let Inventory = {
         };
     }
 };
+
 $(function() {
-    Inventory.Generate(120);
-    Inventory.SetItem(test_item, 7, 0);
-    Inventory.SetItem(test_item, 1, 2,false);
+    Inventory.Generate(122);
+    Inventory.SetItem(item_pdw, 6, 0);
+    Inventory.SetItem(item_parachute, 1, 2,false);
     // Wird nicht angezeigt. IsFree ist false
-    Inventory.SetItem(test_item, 2, 3);
+    Inventory.SetItem(item_hatchet, 3, 3, false);
+
+    $(".slot").droppable({
+        accept: ".item"
+    });
+
+    $(".item").draggable();
 });
+
+/* Aktuell nutze ich pseudo-elements um die Itembilder anzuzeigen.
+ * Es wird etwas benötigt womit man pseudo-elements bearbeiten kann.
+ * Ein möglicher Workaround wäre das variable hinzufügen von <style>-tags in den head:
+ *      $('head').append('<style> .item4x2.combatpdw::before {background: url(...) no-repeat; } </style');
+ *
+ * Andere Möglichkeit wäre alles ins Stylesheet zu packen. Wäre dann aber "hardcoded" und
+ * müsste bei neuen Items angepasst werden.
+ *
+ *
+ */

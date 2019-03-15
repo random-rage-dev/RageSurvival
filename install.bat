2fbf173ecd3513834b4b75412d39534d79515430
Alex <kryptoholyx@googlemail.com>
2019-03-15 03:34:31 +0100

    package etc

--- install.bat
+++ install.bat
@@ -1,7 +1,7 @@
-npm i -g watchify 
-npm i ncp
-npm i node-watch
-npm i fs
-npm i async
-npm i mongoose
-npm i uglifyify
+npm i watchify --save
+npm i ncp --save
+npm i node-watch --save
+npm i fs --save
+npm i async --save
+npm i mongoose --save
+npm i uglifyify --save

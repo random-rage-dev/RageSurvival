54c7b8183bc119d04a08a239cefd471ddcae3ed4
Alex <kryptoholyx@googlemail.com>
2019-04-24 05:39:59 +0200

    storage validate on change & save + index

--- server/world/storage.js
+++ server/world/storage.js
@@ -1,96 +1,157 @@
-const itemIdentity = {
-	"Pump Shotgun": {
-		width: 4,
-		height: 2,
-		max_stack: 1,
-		name: 'Pump Shotgun',
-		image: '../../source/img/weapon_pumpshotgun.png'
-	},
-	"Hatchet": {
-		width: 2,
-		height: 4,
-		max_stack: 1,
-		name: 'Hatchet',
-		image: '../../source/img/hatchet.png'
-	},
-	"12 Gauge Shells": {
-		width: 1,
-		height: 1,
-		max_stack: 32,
-		name: '12 Gauge Shells',
-		image: '../../source/img/12_Gauge_Shells.png'
-	},
-	"Micro SMG": {
-		width: 3,
-		height: 2,
-		max_stack: 1,
-		name: 'Micro SMG',
-		image: '../../source/img/weapon_microsmg.png'
-	},
-	"9mm Bullets": {
-		width: 1,
-		height: 1,
-		max_stack: 128,
-		name: '9mm Bullets',
-		image: '../../source/img/9mm_bullets.png'
-	},
-	"Assault Rifle": {
-		width: 4,
-		height: 2,
-		max_stack: 1,
-		name: 'Assault Rifle',
-		image: '../../source/img/weapon_assaultrifle.png'
-	},
-	"5.56m Bullets": {
-		width: 1,
-		height: 1,
-		max_stack: 64,
-		name: '5.56m Bullets',
-		image: '../../source/img/556m_Bullets.png'
-	},
-	"Drank": {
-		width: 1,
-		height: 1,
-		max_stack: 15,
-		name: 'Drank',
-		image: '../../source/img/energy_drink_small.png'
-	},
-	"Drank Fresh": {
-		width: 1,
-		height: 2,
-		max_stack: 14,
-		name: 'Drank Fresh',
-		image: '../../source/img/energy_drink_small.png'
-	},
-	"Gas Can": {
-		width: 3,
-		height: 3,
-		max_stack: 1,
-		name: 'Gas Can',
-		image: '../../source/img/Icon_jerrycan.png'
-	},
-	"Wood": {
-		width: 3,
-		height: 3,
-		max_stack: 128,
-		name: 'Wood',
-		image: '../../source/img/wood.png'
-	}
-}
+var rpc = require('rage-rpc');
+var itemIdentity = require("./items.js");
 var MongoDB = require("../libs/mongodb.js")
 var Inventory = MongoDB.getInventoryModel();
 var Storage = new class {
 	constructor() {
 		this._tempStorage = [];
 	}
+	log(...msg) {}
 	async validate(player, sStorage, tStorage) {
+		let self = this;
 		console.log("IDS", sStorage.id, tStorage.id);
-		let TempStorage_sStorage = (sStorage.id == "inventory") ? player.class.getInventory() : (this._tempStorage[sStorage.id] || [])
-		/* Validate tStorage*/
-		console.log("TempStorage_sStorage", TempStorage_sStorage);
-		if (sStorage.id != tStorage.id) {
-			/* Validate sStorage*/
-			console.log("validate sStorage");
+		let sStorage_Type = sStorage.id.indexOf("inventory") > -1 ? 'player' : sStorage.id.indexOf("vehicle") > -1 ? "vehicle" : "storage"
+		let tStorage_Type = tStorage.id.indexOf("inventory") > -1 ? 'player' : tStorage.id.indexOf("vehicle") > -1 ? "vehicle" : "storage"
+		let sStorage_ID = sStorage.id.indexOf("inventory") > -1 ? player.class.id : sStorage.id.replace(sStorage_Type, "");
+		let tStorage_ID = tStorage.id.indexOf("inventory") > -1 ? player.class.id : tStorage.id.replace(tStorage_Type, "");
+		let TempStorage_sStorage = (sStorage.id == "inventory") ? player.class.getInventory() : (this._tempStorage[sStorage.id] || []);
+		let TempStorage_tStorage = (tStorage.id == "inventory") ? player.class.getInventory() : (this._tempStorage[tStorage.id] || []);
+		let sStorage_Amount = sStorage.items.reduce(function(total, current) {
+			return total + parseInt(current.amount) || 0;
+		}, 0)
+		let TempStorage_sAmount = TempStorage_sStorage.reduce(function(total, current) {
+			return total + parseInt(current.amount) || 0;
+		}, 0)
+		let tStorage_Amount = tStorage.items.reduce(function(total, current) {
+			return total + parseInt(current.amount) || 0;
+		}, 0)
+		let TempStorage_tAmount = TempStorage_tStorage.reduce(function(total, current) {
+			return total + parseInt(current.amount) || 0;
+		}, 0)
+		console.log(sStorage_Amount, tStorage_Amount, TempStorage_sAmount, TempStorage_tAmount);
+		if ((sStorage_Amount + tStorage_Amount) <= (TempStorage_sAmount + TempStorage_tAmount)) {
+			console.log("Amount check done")
+			/* Validate tStorage*/
+			let items_sStorage = sStorage.items.filter(function(item) {
+				let fItem = TempStorage_sStorage.findIndex(function(cItem) {
+					return (cItem.id == item.id) && (cItem.amount == item.amount);
+				})
+				return fItem == -1;
+			})
+			let items_sStorage_missing = TempStorage_sStorage.filter(function(item) {
+				let fItem = sStorage.items.findIndex(function(cItem) {
+					return (cItem.id == item.id);
+				})
+				return fItem == -1;
+			})
+			console.log("removed Items", JSON.stringify(items_sStorage_missing));
+			console.log("edited Items", JSON.stringify(items_sStorage));
+			try {
+				items_sStorage_missing.forEach(async function(item) {
+					let dStatus = await Inventory.deleteOne({
+						_id: item.id
+					});
+				});
+				items_sStorage.forEach(async function(item) {
+					if (item.id != "NEW") {
+						let inv_update = await Inventory.updateOne({
+							_id: item.id
+						}, {
+							amount: parseInt(item.amount)
+						})
+					} else {
+						let nItem = await new Inventory({
+							owner_type: sStorage_Type,
+							owner_id: sStorage_ID,
+							name: item.name,
+							amount: item.amount,
+							data: item.data
+						}).save();
+						rpc.callBrowsers(player, 'editItemID', {
+							selector: sStorage.id,
+							id: item.id,
+							name: nItem.name,
+							amount: nItem.amount,
+							overwrite_data: {
+								id: nItem._id
+							}
+						}).then((success) => {
+							self.log("Editing Item", nItem._id, "success:", success)
+						});
+					}
+				})
+			} catch (err) {
+				console.log("err", err);
+			}
+			if (sStorage.id != tStorage.id) {
+				/* Validate sStorage*/
+				console.log("validate sStorage");
+				let items_tStorage = tStorage.items.filter(function(item) {
+					let fItem = TempStorage_tStorage.findIndex(function(cItem) {
+						return (cItem.id == item.id) && (cItem.amount == item.amount);
+					})
+					return fItem == -1;
+				})
+				let items_tStorage_missing = TempStorage_tStorage.filter(function(item) {
+					let fItem = tStorage.items.findIndex(function(cItem) {
+						return (cItem.id == item.id);
+					})
+					return fItem == -1;
+				})
+				console.log("removed Items", JSON.stringify(items_tStorage_missing));
+				console.log("edited Items", JSON.stringify(items_tStorage));
+				try {
+					items_tStorage_missing.forEach(async function(item) {
+						let dStatus = await Inventory.deleteOne({
+							_id: item.id
+						});
+					});
+					items_tStorage.forEach(async function(item) {
+						if (item.id != "NEW") {
+							let inv_update = await Inventory.updateOne({
+								_id: item.id
+							}, {
+								amount: parseInt(item.amount)
+							})
+						} else {
+							let nItem = await new Inventory({
+								owner_type: tStorage_Type,
+								owner_id: tStorage_ID,
+								name: item.name,
+								amount: item.amount,
+								data: item.data
+							}).save();
+							rpc.callBrowsers(player, 'editItemID', {
+								selector: tStorage.id,
+								id: item.id,
+								name: nItem.name,
+								amount: nItem.amount,
+								overwrite_data: {
+									id: nItem._id
+								}
+							}).then((success) => {
+								self.log("Editing Item", nItem._id, "success:", success)
+							}).catch((err) => {
+								console.log("err",err);
+							})
+						}
+					})
+				} catch (err) {
+					console.log("err", err);
+				}
+			}
+			self._tempStorage[sStorage.id] = sStorage.items;
+			self._tempStorage[tStorage.id] = tStorage.items;
+
+			if ((sStorage_ID == "inventory") || (tStorage_ID == "inventory")) {
+				let storage = (sStorage_ID == "inventory") ? sStorage.items : tStorage.items;
+				player.class.setInventory(storage);
+			}
+		} else {
+			console.log("Amount misleading, please reload UI");
+			if ((sStorage_ID == "inventory") || (tStorage_ID == "inventory")) {
+				player.class.reloadInventory();
+			}
 		}
 	}
 	async save(player, storage) {}

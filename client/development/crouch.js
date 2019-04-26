0d459574d67deef8b8e77db5a9e4cd50102a8e40
Alex <kryptoholyx@googlemail.com>
2019-04-26 19:44:47 +0200

    -debug

--- client/development/crouch.js
+++ client/development/crouch.js
@@ -1,37 +1,30 @@
 const movementClipSet = "move_ped_crouched";
 const strafeClipSet = "move_ped_crouched_strafing";
 const clipSetSwitchTime = 0.25;
-
 const loadClipSet = (clipSetName) => {
     mp.game.streaming.requestClipSet(clipSetName);
     while (!mp.game.streaming.hasClipSetLoaded(clipSetName)) mp.game.wait(0);
 };
-
 loadClipSet(movementClipSet);
-loadClipSet(strafeClipSet); 
-
+loadClipSet(strafeClipSet);
 mp.events.add("entityStreamIn", (entity) => {
     if (entity.type === "player" && entity.getVariable("isCrouched")) {
         entity.setMovementClipset(movementClipSet, clipSetSwitchTime);
         entity.setStrafeClipset(strafeClipSet);
     }
 });
- 
 mp.events.addDataHandler("isCrouched", (entity, value) => {
     if (entity.type !== "player") return;
-
     if (value) {
         entity.setMovementClipset(movementClipSet, clipSetSwitchTime);
         entity.setStrafeClipset(strafeClipSet);
-
     } else {
         entity.resetMovementClipset(clipSetSwitchTime);
         entity.resetStrafeClipset();
     }
 });
-
 mp.keys.bind(0x11, false, () => {
-    if (mp.canCrouch == true) {
+    if ((mp.canCrouch == true) && (mp.gui.chat.enabled == false)) {
         mp.events.callRemote("Player:Crouch");
     }
 });

d334de8e7d326f3438ad57ca1f29c5e22c2e17cd
Alex <kryptoholyx@googlemail.com>
2019-04-26 19:44:58 +0200

    Storage Saving revamp

--- client/development/storage.js
+++ client/development/storage.js
@@ -12,26 +12,27 @@ var Inventory_Order = {
 };
 var CEFInventory = require("./browser.js").inventory;
 var CEFNotification = require("./browser.js").notification;
-var inv_cells = 0;
-var inv_rows = 0;
-mp.events.add("Inventory:getData", (cell_count, row_count) => {
+CEFInventory.load("interface/index.html");
+var inv_cells = 6;
+var inv_rows = 5;
+let clientWidth = cell_size * inv_cells + (padding * 2)
+let clientHeight = cell_size * inv_rows + 37 + (padding * 2)
+if (mp.storage.data.inventory_order) {
+	let storageData = mp.storage.data.inventory_order;
+	Inventory_Order.positions = storageData.positions || {
+		"inventory": {
+			top: `calc(50% - ${clientHeight/2}px)`,
+			left: `calc(50% - ${clientWidth/2}px)`
+		}
+	};
+	Inventory_Order.items = storageData.items || {};
+} else {
+	mp.storage.data.inventory_order = Inventory_Order;
+}
+mp.events.add("Inventory:Resize", (cell_count, row_count) => {
 	inv_cells = cell_count;
 	inv_rows = row_count;
-	let clientWidth = cell_size * inv_cells + (padding * 2)
-	let clientHeight = cell_size * inv_rows + 37 + (padding * 2)
-	if (mp.storage.data.inventory_order) {
-		let storageData = mp.storage.data.inventory_order;
-		Inventory_Order.positions = storageData.positions || {
-			"inventory": {
-				top: `calc(50% - ${clientHeight/2}px)`,
-				left: `calc(50% - ${clientWidth/2}px)`
-			}
-		};
-		Inventory_Order.items = storageData.items || {};
-	} else {
-		mp.storage.data.inventory_order = Inventory_Order;
-	}
-	CEFInventory.load("interface/index.html");
+	CEFInventory.call("resize", "inventory", inv_cells, inv_rows);
 });
 mp.events.add("Inventory:Ready", (data) => {
 	CEFInventory.call("initialize", inv_cells, inv_rows, {
@@ -43,7 +44,6 @@ mp.events.add("Inventory:Ready", (data) => {
 });
 
 function toggleInventory() {
-	console.log("CAN CROUCH",mp.canCrouch);
 	if (toggleInvState == false) {
 		if (mp.gui.cursor.visible == false) {
 			CEFInventory.call("setPos", "inventory", Inventory_Order.positions["inventory"].top, Inventory_Order.positions["inventory"].left);
@@ -244,17 +244,17 @@ mp.events.add("Storage:AddContainer", (headline, selector, cells, rows, items) =
 		}
 		return gItem;
 	})
-	TempStorage[selector] = gItems;
 	CEFInventory.call("show");
 	CEFInventory.call("setPos", "inventory", Inventory_Order.positions["inventory"].top, Inventory_Order.positions["inventory"].left);
 	let clientWidth = cell_size * cells + (padding * 2)
 	let clientHeight = cell_size * rows + 37 + (padding * 2)
-	let config = {      
+	let config = {
 		top: Inventory_Order.positions[selector] ? Inventory_Order.positions[selector].top : `calc(50% - ${clientHeight/2}px)`,
 		left: Inventory_Order.positions[selector] ? Inventory_Order.positions[selector].left : `calc(50% - ${clientWidth/2}px)`
 	};
 	CEFInventory.call("addStorageContainer", headline, selector, config, cells, rows, gItems);
-	CEFInventory.call("focus",selector);
+	TempStorage[selector] = gItems.map(e => e.item);
+	CEFInventory.call("focus", selector);
 	CEFInventory.cursor(true);
 	toggleInvState = true;
 });
@@ -279,20 +279,99 @@ var StorageSystem = new class {
 		if (TempStorage[source.id]) {
 			sourceTempOld = TempStorage[source.id]
 		}
+		sourceTempOld = sourceTempOld.map(function(e) {
+			return Object.assign(e, {
+				origin: source.id
+			});
+		})
 		if (TempStorage[target.id]) {
 			targetTempOld = TempStorage[target.id]
-		} 
+		}
+		targetTempOld = targetTempOld.map(function(e) {
+			return Object.assign(e, {
+				origin: target.id
+			});
+		})
+		source.items = source.items.map(function(e) {
+			return Object.assign(e, {
+				origin: source.id
+			});
+		})
+		target.items = target.items.map(function(e) {
+			return Object.assign(e, {
+				origin: target.id
+			});
+		})
 		let all_items_temp = (source.id == target.id) ? sourceTempOld : sourceTempOld.concat(targetTempOld); // merge the two temp arrays;
 		let all_items_new = (source.id == target.id) ? source.items : source.items.concat(target.items); // merge the two temp arrays;
+		all_items_temp = all_items_temp.map(function(e) {
+			return {
+				id: e.id,
+				name: e.name,
+				amount: e.amount,
+				origin: e.origin
+			}
+		})
+		all_items_new = all_items_new.map(function(e) {
+			return {
+				id: e.id,
+				name: e.name,
+				amount: e.amount,
+				origin: e.origin
+			}
+		})
+		var toUpdate = false;
 		let temp_Amount = all_items_temp.reduce(function(total, current) {
-			return total + parseInt(current.amount) || 0;
-		}, 0)
+			return total + parseInt(current.amount);
+		}, 0);
 		let new_Amount = all_items_new.reduce(function(total, current) {
-			return total + parseInt(current.amount) || 0;
-		}, 0)
-		/*TODO LOOK OVER needsUpdate*/
+			return total + parseInt(current.amount);
+		}, 0);
+		let toCreate = all_items_new.filter(e => {return e.id == "NEW"});
+
+		if (toCreate.length > 0) {
+			toUpdate = true;
+		}
+		let moved = all_items_new.filter(e => {
+			let fItem = all_items_temp.findIndex(function(cItem) {
+				return (cItem.id == e.id) && ((e.origin != cItem.origin) || (e.amount != cItem.amount));
+			})
+			return (fItem != -1) && (e.id != "NEW");
+		})
+		let removed = all_items_temp.filter(e => {
+			let fItem = all_items_new.findIndex(function(cItem) {
+				return (cItem.id == e.id);
+			})
+			return (fItem == -1) && (e.id != "NEW");
+		})
+		if (removed.length > 0) {
+			toUpdate = true;
+		}
+		moved.forEach(e => {
+			console.log(e.id)
+			let sDoesExist = source.items.findIndex(x => {
+				return x.id == e.id;
+			})
+			let tDoesExist = target.items.findIndex(x => {
+				return x.id == e.id;
+			})
+			if (e.origin != source.id) {
+				if (sDoesExist == -1) {
+					toUpdate = true;
+				}
+			} else if (e.origin != target.id) {
+				if (tDoesExist == -1) {
+					toUpdate = true;
+				}
+			}
+		})
 		console.log("Items Changed target ?", temp_Amount, new_Amount);
-		return (temp_Amount != new_Amount);
+		if (parseInt(temp_Amount) != parseInt(new_Amount)) {
+			toUpdate = true;
+		}
+		console.log("toUpdate", toUpdate);
+		/*TODO LOOK OVER needsUpdate*/
+		return toUpdate;
 	}
 	checkFit(where, w, h) {
 		return new Promise(function(fulfill, reject) {

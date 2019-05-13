989da01d6f91ad0f156e3853eb57f8e786536706
Alex <kryptoholyx@googlemail.com>
2019-05-13 23:29:37 +0200

    weather loading, and applying apocalypse effect

--- client/development/weather.js
+++ client/development/weather.js
@@ -1,24 +1,30 @@
 require("./vector.js");
-
 mp.game.audio.startAudioScene("FBI_HEIST_H5_MUTE_AMBIENCE_SCENE");
 mp.game.audio.startAudioScene("MIC1_RADIO_DISABLE");
-
 var Weather = new class {
     constructor() {
         let self = this;
         self._areas = [];
         self._inside = undefined;
         mp.events.add("Weather:LoadAreas", (weathers) => {
-            self._areas = JSON.parse(weathers);
-            console.log(weathers);
+            self.loadWeather(JSON.parse(weathers));
         });
         setInterval(function() {
             self._check();
         }, 1000);
     }
+    loadWeather(arr) {
+        let self = this;
+        self._areas = arr;
+        self._areas.forEach(function(area, index) {
+            area.polygon.forEach(function(coords, index1) {
+                self._areas[index].polygon[index1] = [coords.x, coords.y];
+            })
+        });
+    }
     enter(key) {
         this._inside = key;
-        let weather = self._areas[key];
+        let weather = this._areas[key];
         mp.game.gameplay.setWind(weather.wind.speed);
         mp.game.gameplay.setWindDirection(weather.wind.dir);
         mp.game.gameplay.setWeatherTypeOverTime(weather.name, 1);
@@ -26,22 +32,28 @@ var Weather = new class {
         mp.events.callRemote("Weather:TransitionTo", this._inside);
     }
     exit() {
+
+        this._inside = undefined;
         mp.events.callRemote("Weather:Exit");
         mp.game.gameplay.setWeatherTypeOverTime("CLEAR", 1);
+        mp.game.gameplay.setWind(0);
+        mp.game.gameplay.setWindDirection(0);
+        mp.game.gameplay.setRainFxIntensity(0);
     }
     _check() {
         console.log("check");
         let self = this;
         if (self._areas.length > 0) {
-            let lp = mp.vector(mp.players.local.position)
+            let lp = mp.vector(mp.players.local.position);
             let inside = self._areas.findIndex(function(area, key) {
-                let inside = lp.insidePolygon(area.polygon);
-                return (inside == true)
+                let inside1 = lp.insidePolygon(area.polygon);
+                console.log("check is inside", inside1);
+                return (inside1 == true)
             })
             console.log("inside", inside);
             if (self._inside != inside) {
                 if (inside > -1) {
-                    self.enter(self._inside);
+                    self.enter(inside);
                 } else {
                     self.exit();
                 }

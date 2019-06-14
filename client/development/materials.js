c2978384f8e2d7060c2bbc6ff85ccf3b9e932564
Alex <kryptoholyx@googlemail.com>
2019-06-14 09:18:09 +0200

    materials, vegetation generation

--- client/development/materials.js
+++ client/development/materials.js
@@ -3,6 +3,7 @@ var materials = {};
 materials[2379541433] = 1;
 materials[127813971] = 2;
 materials[3454750755] = 2;
+materials[581794674] = 3;
 //materials[581794674] = true;
 
 module.exports = materials;
